4ff961190691e6242dd9da2b859d3b093edb5556
Estelle <107674294+EstelleY0@users.noreply.github.com>
2023-07-25 23:44:17 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,60 +1,71 @@
 import cv2
 import numpy as np
 from module import *
+import serial
 
-
-# Calculate slope and direction
-def calculate_slope_and_direction(image, left_lane, right_lane):
-    height, width = image.shape[:2]
-    x1, y1, x2, y2 = left_lane
-    left_slope = (y2 - y1) / (x2 - x1 + 1e-6)
-
-    x1, y1, x2, y2 = right_lane
-    right_slope = (y2 - y1) / (x2 - x1 + 1e-6)
-
-    median_slope = (left_slope + right_slope) / 2
-
-    if median_slope < -0.1:
-        direction = "Left"
-    elif median_slope > 0.1:
-        direction = "Right"
-    else:
-        direction = "Straight"
-
-    return median_slope, direction
+#TODO
+#case of one-side lane detected.
+#written code is based on case of both lane are detected
 
 # Open video capture
 cap = CameraModule(width = 640, height=480)
-cap.open_cam(1)
+cap.open_cam(0)
 
-while True:
+#fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+#out1 = cv2.VideoWriter('drive.mp4', fourcc, 20.0, (640,480))
+
+# serial
+ser = serial.Serial('COM10', 9600)
+
+
+#mode = input()
+mode = '0'
+ser.write(mode.encode())
+
+while mode == '0':
     frame = cap.read()
+    cv2.imshow("original", frame)
 
-    cv2.imshow("Lane Detection", frame)
+    birdeye = imagePreprocess.bird_eye(frame)
+    cv2.imshow("birdeye", birdeye)
+        
     # Apply image processing pipeline
-    edges = imagePreprocess.apply_canny(frame)
-    roi = imagePreprocess.get_roi_vertices(edges)
-    masked = cv2.fillPoly(edges, roi, (0, 0, 0))
-
-    # Get Hough lines and fit lane lines
+    edges = imagePreprocess.apply_canny(birdeye)
+    cv2.imshow("canny", edges)
+    
+    # roi = imagePreprocess.get_roi_vertices(edges)
+    # masked = cv2.fillPoly(edges, roi, (0, 0, 0))
+    # cv2.imshow("mask", masked)
+    masked = edges
+    # roi = edges
+    # Get lane lines, get alane from left/right each
     lines = lineModule.get_lines(masked)
-    left_lane, right_lane = lineModule.classsify_lines(frame, lines)
+    right_lane = lineModule.classify_lines(frame, lines)
 
     # Draw lane lines
-    cv2.line(frame, tuple(left_lane[0][:2]), tuple(left_lane[0][2:]), (0, 0, 255), 5)
-    cv2.line(frame, tuple(right_lane[0][:2]), tuple(right_lane[0][2:]), (0, 0, 255), 5)
-
+    cv2.imshow("linnes", edges)
+    rx1,ry1,rx2,ry2 = right_lane[0]
+    cv2.line(frame, (rx1,ry1),(rx2,ry2), (0, 255,0), 5)
+    
     # Calculate slope and direction
-    slope, direction = calculate_slope_and_direction(frame, left_lane[0], right_lane[0])
+    slope, command = lineModule.get_command(frame, [rx1, ry1, rx2, ry2])
 
     # Display slope and direction on the frame
     cv2.putText(frame, f"Slope: {slope:.2f}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    cv2.putText(frame, f"Direction: {direction}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
+    cv2.putText(frame, f"Command: {command}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
     cv2.imshow("Lane Detection", frame)
+    try:
+        ser.write(str(command).encode())
+    except serial.SerialTimeoutException:
+        ser.write('4'.encode())
+
+    # 동영상 녹화
+    #out1.write(frame)
 
     if cv2.waitKey(1) == ord("q"):
         break
 
 cap.release()
+# ser.close()
 cv2.destroyAllWindows()
